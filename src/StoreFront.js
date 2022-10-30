65892859ac37a48c75b53a891eb1368181bc6f23
Jawid <41361157+infjay@users.noreply.github.com>
2022-10-30 16:39:21 +0100

    event.preventDefault() error fixed

--- src/StoreFront.js
+++ src/StoreFront.js
@@ -1,24 +1,24 @@
+import React,{useState} from "react";
 import Product from "./Product";
-import { useState } from "react";
 
 export default function StoreFront(){
     const [products, setProducts] = useState([])
     const [name, setName] = useState()
     const [description,setDescription] = useState()
 
-function handleFormSubmit(){event=>
+
+const handleFormSubmit= (event)=>{ 
     event.preventDefault()
-    setProducts([...products,{id:products.length + 1,name:name, description:description}])
-    setName("")
-    setDescription("")
+    setProducts([...products,{id:products.length + 1,name:name, description:description}]);
+    setName("");
+    setDescription("");
 }
 
-function handleProductDelete(id){
+const handleProductDelete = (id) => {
     setProducts(products.filter(product => product.id !== id))
 }
 
-return <>
-    <form onSubmit={handleFormSubmit}>
+return <> <form onSubmit={handleFormSubmit}>
         <div>
             <label htmlFor="name">Name :</label>
             <input id="name" value={name} onChange={e => setName(e.target.value)}
@@ -26,8 +26,7 @@ return <>
         </div>
         <div>
             <label htmlFor="description">Description :</label>
-            <input id="description" value={description} onChange={e=>e.target.value} 
-             placeholder="Enter the description" className="textfield" />
+            <input id="description" value={description} onChange={e => setDescription(e.target.value)} placeholder="Enter the description" className="textfield" />
         </div>
         <div className="form-footer">
             <div className="validation-message"></div>
@@ -38,12 +37,11 @@ return <>
         {products.length === 0 && <p>Add your first product</p>}
     </div>
     <ul className="store-front">
-        {products.map(product => 
-        <li key={product.id}>
+        {products.map((product) => 
+        (<li key={product.id}>
         <Product details={product}/>
-        <button onClick={()=>handleProductDelete(product.id)}>Delete</button>
-        </li>)}
+        <button onClick={()=> handleProductDelete(product.id)}>Delete</button>
+        </li>))}
     </ul>
 </>
-
 }
