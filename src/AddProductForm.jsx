650b85649ae9f6aad89e0accf86fa1b4af4f1bd9
Jawid <41361157+infjay@users.noreply.github.com>
2022-11-01 07:07:19 +0100

    props destructured

--- src/AddProductForm.jsx
+++ src/AddProductForm.jsx
@@ -1,17 +1,17 @@
 export const AddProductForm = (props) => {
-
+    const {onFormSubmit,name,onNameChange,description,onDescriptionChange,validation} = props
     return <form onSubmit={props.onFormSubmit}>
     <div>
         <label htmlFor="name">Name :</label>
-        <input id="name" value={props.name} onChange={props.onNameChange}
+        <input id="name" value={name} onChange={onNameChange}
         type="text" placeholder="Enter the name" className="textfield" />
     </div>
     <div>
         <label htmlFor="description">Description :</label>
-        <input id="description" value={props.description} onChange={props.onDescriptionChange} placeholder="Enter the description" className="textfield" />
+        <input id="description" value={description} onChange={onDescriptionChange} placeholder="Enter the description" className="textfield" />
     </div>
     <div className="form-footer">
-        <div className="validation-message">{props.validation}</div>
+        <div className="validation-message">{validation}</div>
         <input type="submit" className="btn btn-primary" value="Add product" />
     </div>
 </form>
