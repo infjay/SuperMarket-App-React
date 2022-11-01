e7a37876180c8ab753b083f8d5598f3bf1587ca5
Jawid <41361157+infjay@users.noreply.github.com>
2022-11-01 07:01:35 +0100

    refactored components added

---
+++ src/StoreFront.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from "react";
+import { AddProductForm } from "./AddProductForm";
+import { ProductList } from "./ProductList";
+
+export default function StoreFront(){
+    const [products, setProducts] = useState([])
+    const [name, setName] = useState("")
+    const [description,setDescription] = useState("")
+    const [validation,setValidation] = useState("")
+
+
+const handleFormSubmit= (event)=>{ 
+    event.preventDefault()
+    if(!name){
+        setValidation("Please enter a name");
+        return;
+    }
+    if(!description){
+        setValidation("Please enter a description");
+        return;
+    }
+    setProducts([...products,{id:products.length + 1,name:name, description:description}]);
+    setName("");
+    setDescription("");
+}
+function handleNameChange(event){
+    setName(event.target.value)
+}
+function handleDescriptionChange(event){
+    setDescription(event.target.value)
+}
+
+const handleProductDelete = (id) => {
+    setProducts(products.filter(product => product.id !== id))
+}
+
+return <> 
+    <AddProductForm name={name} description={description} validation={validation} onNameChange={handleNameChange} onDescriptionChange={handleDescriptionChange} onFormSubmit={handleFormSubmit} />
+    <div>
+        {products.length === 0 && <p>Add your first product</p>}
+    </div>
+    <ProductList products={products} onProductDelete={handleProductDelete} />
+</>
+}
