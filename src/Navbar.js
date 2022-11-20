064908fb416679f4975e7cf1dc34aabee3554214
Jawid <41361157+infjay@users.noreply.github.com>
2022-11-20 19:19:06 +0100

    Navbar added

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { AppContext } from "./AppContext";
+
+export const Navbar = () => {
+    const app = useContext(AppContext) 
+    const handleCurrencyChange = (event)=> {
+        app.setCurrency(event.target.value)
+    }
+    return <>
+    Select Currency <select onChange={handleCurrencyChange}>
+    <option value="EUR">EUR</option>
+    <option value="USD">USD</option>
+    <option value="USD">USD</option>
+    </select> 
+    Current Theme = {app.theme} <button onClick={app.toggleTheme}>Toggle Theme</button>
+    <hr />
+    </>
+}
+
